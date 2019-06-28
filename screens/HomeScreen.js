b2ffef76b809f16556fc5996807e737f42f8b4f3
Ellen <eli030@uottawa.ca>
2019-06-28 16:47:23 -0400

    Add logo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -17,6 +17,7 @@ export default function HomeScreen() {
     <View style={styles.container}>
       <ScrollView
         style={styles.container}
+        
         contentContainerStyle={styles.contentContainer}>
         <View style={styles.welcomeContainer}>
           <Image
